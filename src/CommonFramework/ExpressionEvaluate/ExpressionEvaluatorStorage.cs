08dd778c9972b526f8248e9ae23c822cee64aab2
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-10-03 19:22:27 +0200

    fix ExpressionEvaluatorStorage (#7)

--- src/CommonFramework/ExpressionEvaluate/ExpressionEvaluatorStorage.cs
+++ src/CommonFramework/ExpressionEvaluate/ExpressionEvaluatorStorage.cs
@@ -4,8 +4,8 @@ namespace CommonFramework.ExpressionEvaluate;
 
 public class ExpressionEvaluatorStorage(LambdaCompileMode lambdaCompileMode) : IExpressionEvaluatorStorage
 {
-    private readonly IDictionaryCache<Type, IExpressionEvaluator> cache = new DictionaryCache<Type, IExpressionEvaluator>(_ =>
-        new ExpressionEvaluator(new LambdaCompileCache(lambdaCompileMode)));
+    private readonly IDictionaryCache<Type, IExpressionEvaluator> cache =
+        new DictionaryCache<Type, IExpressionEvaluator>(_ => new ExpressionEvaluator(new LambdaCompileCache(lambdaCompileMode))).WithLock();
 
     public IExpressionEvaluator GetForType(Type type)
     {
