f6b380c51e1532f0c8a7cba5cdb0c4b9dc884baf
iatsuta <98311820+iatsuta@users.noreply.github.com>
2025-11-26 03:11:43 +0100

    add GetEqualityWithExpr (#15)

--- src/CommonFramework/ExpressionHelper.cs
+++ src/CommonFramework/ExpressionHelper.cs
@@ -4,21 +4,40 @@ namespace CommonFramework;
 
 public static class ExpressionHelper
 {
-    public static Expression<Func<T, T>> GetIdentity<T>()
-    {
-        return x => x;
-    }
+	public static Expression<Func<T, TResult>> Create<T, TResult>(Expression<Func<T, TResult>> func)
+	{
+		return func;
+	}
 
-    public static Expression<Func<T, T, bool>> GetEquality<T>()
-    {
-        var p1 = Expression.Parameter(typeof(T));
-        var p2 = Expression.Parameter(typeof(T));
+	public static Expression<Func<T, T>> GetIdentity<T>()
+	{
+		return Cache<T>.IdentityExpr;
+	}
 
-        return Expression.Lambda<Func<T, T, bool>>(Expression.Equal(p1, p2), p1, p2);
-    }
+	public static Expression<Func<T, T, bool>> GetEquality<T>()
+	{
+		return Cache<T>.EqualityExpr;
+	}
 
-    public static Expression<Func<T, TResult>> Create<T, TResult>(Expression<Func<T, TResult>> func)
-    {
-        return func;
-    }
+	public static Expression<Func<T, bool>> GetEqualityWithExpr<T>(T value)
+	{
+		var p1 = Expression.Parameter(typeof(T));
+
+		return Expression.Lambda<Func<T, bool>>(Expression.Equal(p1, Expression.Constant(value)), p1);
+	}
+
+	private static class Cache<T>
+	{
+		public static readonly Expression<Func<T, T>> IdentityExpr = x => x;
+
+		public static readonly Expression<Func<T, T, bool>> EqualityExpr = GetEqualityExpr();
+
+		private static Expression<Func<T, T, bool>> GetEqualityExpr()
+		{
+			var p1 = Expression.Parameter(typeof(T));
+			var p2 = Expression.Parameter(typeof(T));
+
+			return Expression.Lambda<Func<T, T, bool>>(Expression.Equal(p1, p2), p1, p2);
+		}
+	}
 }
