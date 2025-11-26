b73ad88423de31eca5fa6fb73c950114bf2884b1
iatsuta <terraghost@mail.ru>
2025-11-26 03:10:14 +0100

    add GetEqualityWithExpr

--- src/CommonFramework/MethodInfoExtensions.cs
+++ src/CommonFramework/MethodInfoExtensions.cs
@@ -5,40 +5,43 @@ namespace CommonFramework;
 
 public static class MethodInfoExtensions
 {
-    public static TResult Invoke<TResult>(this MethodInfo methodInfo, object source)
+    extension(MethodInfo methodInfo)
     {
-        return methodInfo.Invoke<TResult>(source, []);
-    }
+	    public TResult Invoke<TResult>(object? source)
+	    {
+		    return methodInfo.Invoke<TResult>(source, []);
+	    }
 
-    public static TResult Invoke<TResult>(this MethodInfo methodInfo, object source, IEnumerable<object> args)
-    {
-        return (TResult)methodInfo.Invoke(source, args.ToArray())!;
-    }
+	    public TResult Invoke<TResult>(object? source, IEnumerable<object> args)
+	    {
+		    return (TResult)methodInfo.Invoke(source, args.ToArray())!;
+	    }
 
-    public static TResult Invoke<TResult>(this MethodInfo methodInfo, object source, object arg1, params object[] args)
-    {
-        return methodInfo.Invoke<TResult>(source, new[] { arg1 }.Concat(args));
-    }
+	    public TResult Invoke<TResult>(object? source, object arg1, params object[] args)
+	    {
+		    return methodInfo.Invoke<TResult>(source, new[] { arg1 }.Concat(args));
+	    }
 
-    public static bool IsGenericMethodImplementation(this MethodInfo methodInfo, MethodInfo genericMethod)
-    {
-        if (!genericMethod.IsGenericMethodDefinition)
-        {
-            throw new ArgumentOutOfRangeException(nameof(genericMethod));
-        }
+	    public bool IsGenericMethodImplementation(MethodInfo genericMethod)
+	    {
+		    if (!genericMethod.IsGenericMethodDefinition)
+		    {
+			    throw new ArgumentOutOfRangeException(nameof(genericMethod));
+		    }
 
-        return methodInfo.IsGenericMethod && methodInfo.GetGenericMethodDefinition() == genericMethod;
-    }
+		    return methodInfo.IsGenericMethod && methodInfo.GetGenericMethodDefinition() == genericMethod;
+	    }
 
-    public static MethodCallExpression ToCallExpression(this MethodInfo methodInfo, IEnumerable<Expression> children)
-    {
-        if (methodInfo.IsStatic)
-        {
-            return Expression.Call(methodInfo, children);
-        }
-        else
-        {
-            return children.GetByFirst((first, other) => Expression.Call(first, methodInfo, other));
-        }
+	    public MethodCallExpression ToCallExpression(IEnumerable<Expression> children)
+	    {
+		    if (methodInfo.IsStatic)
+		    {
+			    return Expression.Call(methodInfo, children);
+		    }
+		    else
+		    {
+			    return children.GetByFirst((first, other) => Expression.Call(first, methodInfo, other));
+		    }
+	    }
     }
 }
