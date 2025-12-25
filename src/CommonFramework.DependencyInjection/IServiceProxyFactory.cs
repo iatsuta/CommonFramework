c3f1a35f5caf7d9b9491b8c23b264fc21501e34f
iatsuta <terraghost@mail.ru>
2025-12-25 03:48:03 +0100

    add ServiceProxyFactory

---
+++ src/CommonFramework.DependencyInjection/IServiceProxyFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonFramework.DependencyInjection;
+
+public interface IServiceProxyFactory
+{
+    TService Create<TService>(params object[] args) => this.Create<TService, TService>(args);
+
+    TService Create<TService, TServiceImpl>(params object[] args)
+        where TServiceImpl : TService
+        => this.Create<TService>(typeof(TServiceImpl), args);
+
+    TService Create<TService>(Type requiredService, params object[] args);
+}
