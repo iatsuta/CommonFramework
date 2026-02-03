8428abd25a9146016a85765e8ea90ab6ef230b99
iatsuta <terraghost@mail.ru>
2026-02-03 12:19:48 +0100

    add AlreadyInitialized-di-checks

--- src/CommonFramework.VisualIdentitySource/DependencyInjection/VisualIdentitySourceSettings.cs
+++ src/CommonFramework.VisualIdentitySource/DependencyInjection/VisualIdentitySourceSettings.cs
@@ -8,63 +8,58 @@ namespace CommonFramework.VisualIdentitySource.DependencyInjection;
 
 public class VisualIdentitySourceSettings : IVisualIdentitySourceSettings
 {
-	private VisualIdentityPropertySourceSettings? customSettings;
+    private VisualIdentityPropertySourceSettings? customSettings;
 
-	private readonly List<Action<IServiceCollection>> actions = new();
+    private readonly List<Action<IServiceCollection>> actions = new();
 
-	public IVisualIdentitySourceSettings SetSettings(VisualIdentityPropertySourceSettings settings)
-	{
-		this.customSettings = settings;
+    public IVisualIdentitySourceSettings SetSettings(VisualIdentityPropertySourceSettings settings)
+    {
+        this.customSettings = settings;
 
-		return this;
-	}
+        return this;
+    }
 
-	public IVisualIdentitySourceSettings SetName<TDomainObject>(Expression<Func<TDomainObject, string>> namePath)
-	{
-		var visualIdentityInfo = new VisualIdentityInfo<TDomainObject>(namePath);
+    public IVisualIdentitySourceSettings SetName<TDomainObject>(Expression<Func<TDomainObject, string>> namePath)
+    {
+        var visualIdentityInfo = new VisualIdentityInfo<TDomainObject>(namePath);
 
-		this.actions.Add(sc => sc.AddSingleton(visualIdentityInfo).AddSingleton<VisualIdentityInfo>(visualIdentityInfo));
+        this.actions.Add(sc => sc.AddSingleton(visualIdentityInfo).AddSingleton<VisualIdentityInfo>(visualIdentityInfo));
 
-		return this;
-	}
+        return this;
+    }
 
-	public IVisualIdentitySourceSettings SetDisplay<TDomainObject>(Func<TDomainObject, string> displayFunc)
-		where TDomainObject : class
-	{
-		var displayObjectInfo = new DisplayObjectInfo<TDomainObject>(displayFunc);
+    public IVisualIdentitySourceSettings SetDisplay<TDomainObject>(Func<TDomainObject, string> displayFunc)
+        where TDomainObject : class
+    {
+        var displayObjectInfo = new DisplayObjectInfo<TDomainObject>(displayFunc);
 
-		this.actions.Add(sc => sc.AddSingleton(displayObjectInfo).AddSingleton<DisplayObjectInfo>(displayObjectInfo));
+        this.actions.Add(sc => sc.AddSingleton(displayObjectInfo).AddSingleton<DisplayObjectInfo>(displayObjectInfo));
 
-		return this;
-	}
+        return this;
+    }
 
-	public void Initialize(IServiceCollection services)
-	{
-		if (this.AlreadyInitialized(services))
-		{
-			if (this.customSettings != null)
-			{
-				services.ReplaceSingleton(this.customSettings);
-			}
-		}
-		else
-		{
-			services.AddSingleton<IVisualIdentityInfoSource, VisualIdentityInfoSource>();
-			services.AddSingleton<IVisualIdentityPropertyExtractor, VisualIdentityPropertyExtractor>();
+    public void Initialize(IServiceCollection services)
+    {
+        if (services.AlreadyInitialized<IVisualIdentityInfoSource>())
+        {
+            if (this.customSettings != null)
+            {
+                services.ReplaceSingleton(this.customSettings);
+            }
+        }
+        else
+        {
+            services.AddSingleton<IVisualIdentityInfoSource, VisualIdentityInfoSource>();
+            services.AddSingleton<IVisualIdentityPropertyExtractor, VisualIdentityPropertyExtractor>();
 
-			services.AddSingleton<IDomainObjectDisplayService, DomainObjectDisplayService>();
+            services.AddSingleton<IDomainObjectDisplayService, DomainObjectDisplayService>();
 
-			services.AddSingleton(this.customSettings ?? VisualIdentityPropertySourceSettings.Default);
-		}
+            services.AddSingleton(this.customSettings ?? VisualIdentityPropertySourceSettings.Default);
+        }
 
-		foreach (var action in this.actions)
-		{
-			action(services);
-		}
-	}
-
-	private bool AlreadyInitialized(IServiceCollection services)
-	{
-		return services.Any(sd => !sd.IsKeyedService && sd.Lifetime == ServiceLifetime.Singleton && sd.ServiceType == typeof(IVisualIdentityInfoSource));
-	}
+        foreach (var action in this.actions)
+        {
+            action(services);
+        }
+    }
 }
