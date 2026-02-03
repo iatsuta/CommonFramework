513c066d32cb0c83379d19260459ec925f1a5f21
iatsuta <terraghost@mail.ru>
2026-02-03 11:31:51 +0100

    add CommonFramework.DependencyInjection.Abstractions

--- src/CommonFramework/PropertyAccessors.cs
+++ src/CommonFramework/PropertyAccessors.cs
@@ -1,16 +1,26 @@
 ﻿using System.Linq.Expressions;
 
+using CommonFramework.ExpressionComparers;
+
 namespace CommonFramework;
 
 public record PropertyAccessors<TSource, TProperty>(
     Expression<Func<TSource, TProperty>> Path,
     Func<TSource, TProperty> Getter,
     Action<TSource, TProperty> Setter)
-    : ReadonlyPropertyAccessors<TSource, TProperty>(Path, Getter)
 {
     public PropertyAccessors(
         Expression<Func<TSource, TProperty>> path)
         : this(path, path.Compile(), path.ToSetLambdaExpression().Compile())
     {
+    }
+
+    public virtual bool Equals(PropertyAccessors<TSource, TProperty>? other) =>
+        object.ReferenceEquals(this, other)
+        || (other is not null && ExpressionComparer.Default.Equals(this.Path, other.Path));
+
+    public override int GetHashCode()
+    {
+        return ExpressionComparer.Default.GetHashCode(this.Path);
     }
 }
